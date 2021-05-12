04f128c1865d044ee3e49cc37e5f431db0bdd022
CinqMilleAns <cinq.mille.ans@gmail.com>
2021-05-12 18:15:51 +0200

    Dictionary

--- Files/UserControls/Search/SearchSettingMenu.xaml.cs
+++ Files/UserControls/Search/SearchSettingMenu.xaml.cs
@@ -11,17 +11,21 @@ namespace Files.UserControls.Search
 {
     public sealed partial class SearchSettingMenu : UserControl
     {
+        private readonly FilterDictionary Filters;
+
         public SearchSettingMenu()
         {
             this.InitializeComponent();
-        }
 
-        private readonly FilterCollection Filters = new FilterCollection
-        {
-            new DateFilter("Date de création", "creationDate"),
-            new StringFilter("Artist", "artist"),
-            new ComparableFilter<ulong>("Size", "size"),
-        };
+            var filters = new List<ICriteriaFilter>
+            {
+                new DateFilter("creationDate", "Date de création"),
+                new StringFilter("artist", "Artist"),
+                new ComparableFilter<ulong>("Size", "size"),
+            };
+
+            Filters = new FilterDictionary(filters.ToDictionary(filter => filter.Code, filter => (IFilter)filter));
+        }
 
         private interface IFilter
         {
@@ -29,23 +33,56 @@ namespace Files.UserControls.Search
         }
         private interface ICriteriaFilter : IFilter
         {
+            string Code { get; }
             string Label { get; }
         }
 
-        private class FilterCollection : Collection<IFilter>, IFilter
+        private class FilterDictionary : Dictionary<string, IFilter>
         {
-            public FilterCollection() : base()
+            public FilterDictionary() : base()
             {
             }
-            public FilterCollection(IList<IFilter> filters) : base(filters)
+            public FilterDictionary(IDictionary<string, IFilter> dictionary) : base(dictionary)
+            {
+            }
+            public FilterDictionary(int capacity) : base(capacity)
+            {
+            }
+            public FilterDictionary(IEnumerable<KeyValuePair<string, IFilter>> collection) : base(collection)
+            {
+            }
+            public FilterDictionary(IEqualityComparer<string> comparer) : base(comparer)
+            {
+            }
+            public FilterDictionary(int capacity, IEqualityComparer<string> comparer) : base(capacity, comparer)
+            {
+            }
+            public FilterDictionary(IDictionary<string, IFilter> dictionary, IEqualityComparer<string> comparer) : base(dictionary, comparer)
+            {
+            }
+            public FilterDictionary(IEnumerable<KeyValuePair<string, IFilter>> collection, IEqualityComparer<string> comparer) : base(collection, comparer)
             {
             }
 
             public string ToFilterString()
-                => string.Join(' ', Items.Select(item => item.ToFilterString()));
+                => string.Join(' ', this.Values.Select(item => item.ToFilterString()));
         }
 
-        private class ComparableFilter<T> : ObservableObject, ICriteriaFilter where T : IComparable<T>
+        private abstract class CriteriaFilter : ObservableObject, ICriteriaFilter
+        {
+            public string Code { get; }
+            public string Label { get; }
+
+            public CriteriaFilter(string code, string label)
+            {
+                Code = code;
+                Label = label;
+            }
+
+            public abstract string ToFilterString();
+        }
+
+        private class ComparableFilter<T> : CriteriaFilter, ICriteriaFilter where T : IComparable<T>
         {
             public enum Comparators : ushort
             {
@@ -58,54 +95,47 @@ namespace Files.UserControls.Search
             }
 
             private Comparators comparator = Comparators.EqualTo;
-
-            private T minValue;
-            private T maxValue;
-
-            private readonly string field;
-
-            public string Label { get; }
-
             public Comparators Comparator
             {
                 get => comparator;
                 set => SetProperty(ref comparator, value);
             }
 
+            private T minValue;
             public T MinValue
             {
                 get => minValue;
                 set => SetProperty(ref minValue, value);
             }
+
+            private T maxValue;
             public T MaxValue
             {
                 get => maxValue;
                 set => SetProperty(ref maxValue, value);
             }
 
-            public ComparableFilter(string label, string field)
+            public ComparableFilter(string code, string label) : base(code, label)
             {
-                Label = label;
-                this.field = field;
             }
 
-            public string ToFilterString()
+            public override string ToFilterString()
             {
                 return Comparator switch
                 {
                     Comparators.None => string.Empty,
 
-                    Comparators.EqualTo => $"{field}:={minValue}",
-                    Comparators.LessThan => $"{field}:<={maxValue}",
-                    Comparators.GreaterThan => $"{field}:>={minValue}",
-                    Comparators.Between => $"{field}:>={minValue} {field}:<={maxValue}",
+                    Comparators.EqualTo => $"{Code}:={MinValue}",
+                    Comparators.LessThan => $"{Code}:<={MaxValue}",
+                    Comparators.GreaterThan => $"{Code}:>={MinValue}",
+                    Comparators.Between => $"{Code}:>={MinValue} {Code}:<={MaxValue}",
 
                     _ => string.Empty
                 };
             }
         }
 
-        private class StringFilter : ObservableObject, ICriteriaFilter
+        private class StringFilter : CriteriaFilter
         {
             public enum Comparators : ushort
             {
@@ -118,48 +148,40 @@ namespace Files.UserControls.Search
             }
 
             private Comparators comparator = Comparators.EqualTo;
-
-            private string value;
-
-            private readonly string field;
-
-            public string Label { get; }
-
             public Comparators Comparator
             {
                 get => comparator;
                 set => SetProperty(ref comparator, value);
             }
 
+            private string value;
             public string Value
             {
                 get => value;
                 set => SetProperty(ref this.value, value);
             }
 
-            public StringFilter(string label, string field)
+            public StringFilter(string code, string label) : base(code, label)
             {
-                Label = label;
-                this.field = field;
             }
 
-            public string ToFilterString()
+            public override string ToFilterString()
             {
                 return Comparator switch
                 {
                     Comparators.None => string.Empty,
 
-                    Comparators.EqualTo => $"{field}:={value}",
-                    Comparators.StartsWith => $"{field}:={value}",
-                    Comparators.EndsWith => $"{field}:={value}",
-                    Comparators.Contains => $"{field}:={value}",
+                    Comparators.EqualTo => $"{Code}:={Value}",
+                    Comparators.StartsWith => $"{Code}:={Value}",
+                    Comparators.EndsWith => $"{Code}:={Value}",
+                    Comparators.Contains => $"{Code}:={Value}",
 
                     _ => string.Empty
                 };
             }
         }
 
-        private class DateFilter : ObservableObject, ICriteriaFilter
+        private class DateFilter : CriteriaFilter
         {
             public enum Comparators : ushort
             {
@@ -177,52 +199,45 @@ namespace Files.UserControls.Search
             }
 
             private Comparators comparator = Comparators.EqualTo;
-
-            private DateTime minDate = DateTime.MinValue;
-            private DateTime maxDate = DateTime.MaxValue;
-
-            private readonly string field;
-
-            public string Label { get; }
-
             public Comparators Comparator
             {
                 get => comparator;
                 set => SetProperty(ref comparator, value);
             }
 
+            private DateTime maxDate = DateTime.MaxValue;
             public DateTime MinDate
             {
                 get => minDate;
                 set => SetProperty(ref minDate, value);
             }
+
+            private DateTime minDate = DateTime.MinValue;
             public DateTime MaxDate
             {
                 get => maxDate;
                 set => SetProperty(ref maxDate, value);
             }
 
-            public DateFilter(string label, string field)
+            public DateFilter(string code, string label) : base(code, label)
             {
-                Label = label;
-                this.field = field;
             }
 
-            public string ToFilterString()
+            public override string ToFilterString()
             {
                 return Comparator switch
                 {
                     Comparators.None => string.Empty,
 
-                    Comparators.EqualTo => $"{field}:={minDate:yyyy/MM/dd}",
-                    Comparators.Before => $"{field}:<={maxDate:yyyy/MM/dd}",
-                    Comparators.After => $"{field}:>={minDate:yyyy/MM/dd}",
-                    Comparators.Between => $"{field}:>={minDate:yyyy/MM/dd} {field}:<={maxDate:yyyy/MM/dd}",
+                    Comparators.EqualTo => $"{Code}:={MinDate:yyyy/MM/dd}",
+                    Comparators.Before => $"{Code}:<={MaxDate:yyyy/MM/dd}",
+                    Comparators.After => $"{Code}:>={MinDate:yyyy/MM/dd}",
+                    Comparators.Between => $"{Code}:>={MinDate:yyyy/MM/dd} {Code}:<={MaxDate:yyyy/MM/dd}",
 
-                    Comparators.DayAgo => $"{field}:={DateTime.Now.Date.AddDays(-1):yyyy/MM/dd}",
-                    Comparators.WeekAgo => $"{field}:={DateTime.Now.Date.AddDays(-7):yyyy/MM/dd}",
-                    Comparators.MonthAgo => $"{field}:={DateTime.Now.Date.AddMonths(-1):yyyy/MM/dd}",
-                    Comparators.YearAgo => $"{field}:={DateTime.Now.Date.AddYears(-1):yyyy/MM/dd}",
+                    Comparators.DayAgo => $"{Code}:={DateTime.Now.Date.AddDays(-1):yyyy/MM/dd}",
+                    Comparators.WeekAgo => $"{Code}:={DateTime.Now.Date.AddDays(-7):yyyy/MM/dd}",
+                    Comparators.MonthAgo => $"{Code}:={DateTime.Now.Date.AddMonths(-1):yyyy/MM/dd}",
+                    Comparators.YearAgo => $"{Code}:={DateTime.Now.Date.AddYears(-1):yyyy/MM/dd}",
 
                     _ => string.Empty
                 };
