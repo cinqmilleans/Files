f086a186033bc264387d963d56ad334e6c31507d
CinqMilleAns <cinq.mille.ans@gmail.com>
2021-05-16 17:35:57 +0200

    Init

---
+++ Files/Filesystem/Search/FolderSearchOption.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Windows.Storage.Search;
+
+namespace Files.Filesystem.Search
+{
+    public interface IFolderSearchParameter
+    {
+        string Value { get; }
+        void ApplyParameter(QueryOptions option);
+    }
+    public interface INamedFolderSearchParameter : IFolderSearchParameter
+    {
+        string Name { get; }
+    }
+
+    public class FolderSearchOption
+    {
+        public List<IFolderSearchParameter> Parameters = new List<IFolderSearchParameter>();
+
+        public QueryOptions ToQueryOptions()
+        {
+            var option = new QueryOptions { UserSearchFilter = string.Empty };
+
+            foreach (var parameter in Parameters)
+                parameter.ApplyParameter(option);
+
+            option.UserSearchFilter = option.UserSearchFilter.Trim();
+
+            return option;
+        }
+    }
+
+    public class QueryFolderSearchParameter : IFolderSearchParameter
+    {
+        public string Value { get; set; } = string.Empty;
+
+        public void ApplyParameter(QueryOptions option)
+        {
+            option.UserSearchFilter += $" {Value}";
+        }
+    }
+
+    public abstract class FilterFolderSearchParameter : INamedFolderSearchParameter
+    {
+        public string Name { get; }
+        public string Value { get; set; }
+
+        public FilterFolderSearchParameter(string name)
+        {
+            Name = name;
+        }
+
+        public void ApplyParameter(QueryOptions option)
+        {
+            option.UserSearchFilter += $" {Name}:{Value}";
+        }
+    }
+
+    public class PeriodFolderSearchParameter : FilterFolderSearchParameter
+    {
+        public PeriodFolderSearchParameter (string name) : base(name)
+        {
+        }
+    }
+
+    public enum Period
+    {
+        None,
+        DayAgo,
+        WeekAgo,
+        MonthAgo,
+        YearAgo,
+    }
+}
---
+++ Files/UserControls/Search/SearchControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Files.Filesystem.Search;
+using System.Linq;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Files.UserControls.Search
+{
+    public sealed partial class SearchControl : UserControl
+    {
+        public SearchControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+    public class TextBoxEx : RichEditBox
+    {
+        private readonly FolderSearchOption option = new FolderSearchOption();
+
+        public TextBoxEx()
+        {
+            TextChanged += TextBoxEx_TextChanged;
+        }
+
+        private void TextBoxEx_TextChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            var text = string.Join(" ", option.Parameters.Select(parameter => ToRichTextFormat(parameter)));
+            Document.SetText(TextSetOptions.FormatRtf, @"{\rtf1\ansi " + text + "}");
+
+            static string ToRichTextFormat (IFolderSearchParameter parameter)
+            {
+                if (parameter is INamedFolderSearchParameter named)
+                {
+                    return $"\\b {named.Name} \\b0 : \\b {named.Value} \\b0";
+                }
+                return parameter.Value;
+            }
+        }
+    }
+}
