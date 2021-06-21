7a8f56445475c95114d5f5d20e863df15b3def9b
CinqMilleAns <cinq.mille.ans@gmail.com>
2021-06-21 22:54:53 +0200

    Test

---
+++ Files/Filesystem/Search/SearchOptionKey.cs
@@ -0,0 +1,51 @@
+﻿namespace Files.Filesystem.Search
+{
+    public interface ISearchOptionKey
+    {
+        string Text { get; }
+        string Label { get; }
+
+        IFactory<ISearchOptionValue> ValueFactory { get; }
+
+        string[] Suggestions { get; }
+        string GetAdvancedQuerySyntax(ISearchOptionValue value);
+    }
+
+    public class DateSearchOptionKey : ISearchOptionKey
+    {
+        public virtual string Text => "date";
+        public virtual string Label => "Date of creation";
+
+        public IFactory<ISearchOptionValue> ValueFactory { get; } = IntervalPeriodSearchOptionValueFactory.Default;
+
+        public string[] Suggestions { get; } = new string[] { "modified:", "date:2019" };
+
+        public string GetAdvancedQuerySyntax(ISearchOptionValue value)
+        {
+            if (value is IAdvancedQuerySyntaxValue aqsValue)
+            {
+                return $"System.ItemDate:{aqsValue.AdvancedQuerySyntax}";
+            }
+            return string.Empty;
+        }
+    }
+
+    public class ModifiedSearchOptionKey : ISearchOptionKey
+    {
+        public virtual string Text => "modified";
+        public virtual string Label => "Date of last modification";
+
+        public IFactory<ISearchOptionValue> ValueFactory { get; } = IntervalPeriodSearchOptionValueFactory.Default;
+
+        public string[] Suggestions { get; } = new string[0];
+
+        public string GetAdvancedQuerySyntax(ISearchOptionValue value)
+        {
+            if (value is IAdvancedQuerySyntaxValue aqsValue)
+            {
+                return $"System.DateModified:{aqsValue.AdvancedQuerySyntax}";
+            }
+            return string.Empty;
+        }
+    }
+}
