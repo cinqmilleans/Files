d2ce7210d9a503115bae72f9e6bea70c5f1b74d8
cinqmilleans <cinq.mille.ans@gmail.com>
2022-01-05 17:23:11 +0100

    header manager

--- src/Files/Filesystem/Search/SearchFilter.cs
+++ src/Files/Filesystem/Search/SearchFilter.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
 
 namespace Files.Filesystem.Search
 {
-    public interface ISearchHeaderManager
-    {
-        bool HasHeader(string key);
-        ISearchHeader GetHeader(string key);
-    }
-
     public interface ISearchHeader
     {
         string Key { get; }
@@ -42,29 +34,4 @@ namespace Files.Filesystem.Search
 
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class SearchHeaderAttribute : Attribute {}
-
-    public class SearchHeaderManager : ISearchHeaderManager
-    {
-        private readonly IReadOnlyDictionary<string, ISearchHeader> headers = GetHeaders().ToDictionary(header => header.Key);
-
-        public bool HasHeader(string key) => headers.ContainsKey(key);
-        public ISearchHeader GetHeader(string key) => headers[key];
-
-        private static IEnumerable<ISearchHeader> GetHeaders()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            foreach (Type type in assembly.GetTypes())
-            {
-                var attributes = type.GetCustomAttributes(typeof(SearchHeaderAttribute), false);
-                if (attributes.Length == 1)
-                {
-                    var header = Activator.CreateInstance(type) as ISearchHeader;
-                    if (header is not null)
-                    {
-                        yield return header;
-                    }
-                }
-            }
-        }
-    }
 }
--- src/Files/Filesystem/Search/SearchSettings.cs
+++ src/Files/Filesystem/Search/SearchSettings.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 
 namespace Files.Filesystem.Search
 {
@@ -12,6 +16,8 @@ namespace Files.Filesystem.Search
 
     public class SearchSettings : ObservableObject, ISearchSettings
     {
+        public IReadOnlyList<ISearchHeader> Headers { get; } = GetHeaders().ToList().AsReadOnly();
+
         public bool searchInSubFolders = true;
         public bool SearchInSubFolders
         {
@@ -20,5 +26,32 @@ namespace Files.Filesystem.Search
         }
 
         public SearchFilterCollection Filter { get; } = new SearchFilterCollection(GroupAggregates.And);
+
+        public SearchSettings()
+        {
+            var pinnedKeys = new string[] { "size", "modified" };
+
+            var filters = Headers
+                .Where(header => pinnedKeys.Contains(header.Key))
+                .Select(header => header.GetFilter()).ToList();
+            Filter = new SearchFilterCollection(GroupAggregates.And, filters);
+        }
+
+        private static IEnumerable<ISearchHeader> GetHeaders()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            foreach (Type type in assembly.GetTypes())
+            {
+                var attributes = type.GetCustomAttributes(typeof(SearchHeaderAttribute), false);
+                if (attributes.Length == 1)
+                {
+                    var header = Activator.CreateInstance(type) as ISearchHeader;
+                    if (header is not null)
+                    {
+                        yield return header;
+                    }
+                }
+            }
+        }
     }
 }
