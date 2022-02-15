2e3130e35b109d90fa9b4e48070dee6b26989051
cinqmilleans <cinq.mille.ans@gmail.com>
2022-02-15 20:24:12 +0100

    fix header

--- src/Files/UserControls/Search/SearchFilterPicker.xaml.cs
+++ src/Files/UserControls/Search/SearchFilterPicker.xaml.cs
@@ -57,7 +57,7 @@ namespace Files.UserControls.Search
 
             MenuFlyoutItem GetItem(ISearchHeader header) => new()
             {
-                Tag = header,
+                Tag = new SearchHeaderViewModel(header),
                 Template = HeaderItemTemplate,
             };
         }
