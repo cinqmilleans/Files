5ca4d0710f33a8531d867a79947909948417d4ca
cinqmilleans <cinq.mille.ans@gmail.com>
2022-07-18 18:04:58 +0200

    ShortKey

---
+++ src/Files.Uwp/Helpers/ShortKey/SettingShortKey.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
+
+namespace Files.Uwp.Helpers
+{
+    [MarkupExtensionReturnType(ReturnType = KeyboardAccelerator)]
+    public sealed class SettingShortKey : MarkupExtension
+    {
+        private static ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public bool IsEnabled { get; }
+
+        protected override object ProvideValue()
+        {
+            return resourceLoader.GetString(this.Name);
+        }
+    }
+
+}
