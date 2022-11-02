3b1d5f2d217fdd087a768f56d09f94cf9613cdcc
cinqmilleans <cinq.mille.ans@gmail.com>
2022-11-02 20:43:52 +0100

    tmp

--- src/Files.App/UserControls/Key/ShortKeyPicker.xaml.cs
+++ src/Files.App/UserControls/Key/ShortKeyPicker.xaml.cs
@@ -1,9 +1,26 @@
+using Files.App.ViewModels.Key;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace Files.App.UserControls
 {
 	public sealed partial class ShortKeyPicker : UserControl
 	{
+		public static readonly DependencyProperty ShortKeyProperty =
+			DependencyProperty.Register(nameof(ShortKey), typeof(ShortKey), typeof(ShortKeyPicker), new PropertyMetadata(ShortKey.None));
+
+		public ShortKey ShortKey
+		{
+			get => (ShortKey)GetValue(ShortKeyProperty);
+			set => SetValue(ShortKeyProperty, value);
+		}
+
 		public ShortKeyPicker() => InitializeComponent();
+
+		private void Picker_PreviewKeyDown(object _, KeyRoutedEventArgs e)
+		{
+			e.Handled = true;
+		}
 	}
 }
